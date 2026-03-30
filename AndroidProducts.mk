eea7c9d623281107a79c0950a9547babf73ce172
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2026-03-30 14:50:43 -0400

    beyond1lte: Project-Matrixx bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond1lte.mk
+    $(LOCAL_DIR)/matrixx_beyond1lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond1lte-eng \
-    lineage_beyond1lte-user \
-    lineage_beyond1lte-userdebug
+    matrixx_beyond1lte-eng \
+    matrixx_beyond1lte-user \
+    matrixx_beyond1lte-userdebug
